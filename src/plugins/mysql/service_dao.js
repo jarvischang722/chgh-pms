3b3555e261ced402aaa7fbe2e6d95a5df93094be
a14017 <chang.maz@athena.com.tw>
2017-04-06 00:51:09 +0800

    pms 振興電子白板套版

--- src/plugins/mysql/service_dao.js
+++ src/plugins/mysql/service_dao.js
@@ -7,11 +7,12 @@ var fs = require("fs");
 var commonTools = require("../../utils/commonTools");
 var path = require('path');
 var daoDirectory = __dirname+"/dao/";
+var _ = require("underscore");
 var _str = require("underscore.string");
 
 fs.readdirSync(daoDirectory).filter(function(file) {
 
-     all_dao = commonTools.mergeObject(all_dao, require("./dao/" + file));
+     all_dao =  _.extend(all_dao, require("./dao/" + file));
 
 });
 
--- src/public/js/common_params.js
+++
@@ -1,5 +0,0 @@
-
-
-var global_ward_zone_id;
-var global_system_type;
-
