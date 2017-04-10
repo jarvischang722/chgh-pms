ffaa5da1188b1bca01a4286771637a360edd75dc
Jun Chang <mazhorse722@gmail.com>
2017-04-11 02:15:55 +0800

    模擬API

---
+++ src/controllers/DashBoardAPIController.js
@@ -0,0 +1,110 @@
+/**
+ * Created by Jun on 2017/4/11.
+ * 模擬振興API
+ */
+
+var _ = require("underscore");
+var request = require('request');
+var parseString = require('xml2js').parseString;
+var tools = require("../utils/commonTools");
+var moment = require("moment");
+var fs = require("fs");
+
+/**
+ *取得病房所有病床資訊
+ */
+exports.Get_nur_Patient = function(req, res){
+    fs.readFile(__dirname+'/../testData/NurPatient.xml', 'utf8', function(err, apiResult) {
+        res.header('Content-Type', 'text/xml').send(apiResult);
+    });
+};
+
+/**
+ *取得前一日動態表資料
+ */
+exports.day_before_info = function(req, res){
+    fs.readFile(__dirname+'/../testData/DayBeforeInfo.xml', 'utf8', function(err, apiResult) {
+        res.header('Content-Type', 'text/xml').send(apiResult);
+    });
+};
+
+/**
+ *取得入院、轉入院資料
+ */
+exports.In_TranIn_Data = function(req, res){
+    fs.readFile(__dirname+'/../testData/InTranInData.xml', 'utf8', function(err, apiResult) {
+        res.header('Content-Type', 'text/xml').send(apiResult);
+    });
+};
+
+/**
+ * 取得出院、轉出資料
+ */
+exports.Out_TranOut_Data = function(req, res){
+    fs.readFile(__dirname+'/../testData/OutTranOutData.xml', 'utf8', function(err, apiResult) {
+        res.header('Content-Type', 'text/xml').send(apiResult);
+    });
+};
+
+/**
+ *取得床位數資訊
+ */
+exports.nur_bed_info = function(req, res){
+    fs.readFile(__dirname+'/../testData/NurBedInfo.xml', 'utf8', function(err, apiResult) {
+        res.header('Content-Type', 'text/xml').send(apiResult);
+    });
+};
+
+/**
+ *手術排程資訊
+ */
+exports.op_schedule_info = function(req, res){
+    fs.readFile(__dirname+'/../testData/OpScheduleInfo.xml', 'utf8', function(err, apiResult) {
+        res.header('Content-Type', 'text/xml').send(apiResult);
+    });
+};
+
+/**
+ *檢查排程資訊
+ */
+exports.exam_schedule_info = function(req, res){
+    fs.readFile(__dirname+'/../testData/ExamScheduleInfo.xml', 'utf8', function(err, apiResult) {
+        res.header('Content-Type', 'text/xml').send(apiResult);
+    });
+};
+
+/**
+ *護理排班資訊
+ */
+exports.nis_duty_schedule = function(req, res){
+    fs.readFile(__dirname+'/../testData/NisDutySchedule.xml', 'utf8', function(err, apiResult) {
+        res.header('Content-Type', 'text/xml').send(apiResult);
+    });
+};
+
+/**
+ *病患過敏資訊
+ */
+exports.Get_Allergy_Data = function(req, res){
+    fs.readFile(__dirname+'/../testData/AllergyData.xml', 'utf8', function(err, apiResult) {
+        res.header('Content-Type', 'text/xml').send(apiResult);
+    });
+};
+
+/**
+ *醫師與PA資訊
+ */
+exports.RetrieveVS = function(req, res){
+    fs.readFile(__dirname+'/../testData/RetrieveVS.json', 'json', function(err, apiResult) {
+        res.json(apiResult);
+    });
+};
+
+/**
+ *各科值班表
+ */
+exports.ShiftCollectList = function(req, res){
+    fs.readFile(__dirname+'/../testData/ShiftCollectList.xml', 'utf8', function(err, apiResult) {
+        res.header('Content-Type', 'text/xml').send(apiResult);
+    });
+};
--- src/test/testXmlParse.js
+++ src/test/testXmlParse.js
@@ -5,7 +5,8 @@ var fs = require("fs");
 var parseString = require('xml2js').parseString;
 var xml = "<root>Hello xml2js!</root>"
 var svc  = require("../services/DashBoardWebService");
-
+var request = require('request');
+var config = require("../configs/SystemConfig");
 // fs.readFile('../testData/InTranInData.xml', 'utf8', function(err, xmlData) {
 //
 //     parseString(xmlData, {trim: true,ignoreAttrs:true}, function (err, result) {
@@ -13,11 +14,20 @@ var svc  = require("../services/DashBoardWebService");
 //     });
 //
 // });
-var params = {
+var formData = {
     StratDate: '20170301',
     EndDate:'20170301'
 };
-svc.getOpScheduleInfo(params,function (err,data) {
-    console.error(err);
+// request.post({url:config.web_service_url+"day_before_info", form: formData},function (error, response, body) {
+//     console.log(error);
+//     console.log(body);
+// });
+
+
+
+svc.getRetrieveVS(formData,function (err,data) {
+    if(err)
+        console.error(err);
+
     console.dir(data);
 })
